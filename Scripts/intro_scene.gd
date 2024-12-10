5946e6690d95a8656af37b112ca4bcc976f94f38
ZakharSh <zakhar20032q@gmail.com>
2024-12-10 15:40:39 +0200

    IntroScene

---
+++ Scripts/intro_scene.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _ready():
+	$Label.text = """ Сьогоднішня ситуація у світі:
+ - Війна, розруха і тисячі людей намагаються врятуватися.
+ - Вам доручено провести перевірку на кордоні України та Польщі."""
+	# Настройка кнопки
+	#$Button.text = "Продовжити"
+	#$Button.connect("pressed", self, "_on_continue_pressed")
+
+func _on_continue_pressed():
+	# Переход к сцене с рабочим местом
+	get_tree().change_scene_to_file("res://Scenes/GameScene.tscn")
--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _on_StartGame_pressed():
 	# Убедитесь, что путь к сцене корректен
-	get_tree().change_scene_to_file("res://Scenes/GameScene.tscn")
+	get_tree().change_scene_to_file("res://Scenes/IntroScene.tscn")
