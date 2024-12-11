434e26afce33ec46f5c3089992347e45696ce4a8
ZakharSh <zakhar20032q@gmail.com>
2024-12-11 23:37:35 +0200

    IntroScene

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -3,3 +3,6 @@ extends Control
 func _on_StartGame_pressed():
 	# Убедитесь, что путь к сцене корректен
 	get_tree().change_scene_to_file("res://Scenes/IntroScene.tscn")
+
+func _on_Exit_pressed():
+	get_tree().quit()
