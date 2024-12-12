6935063e6fc563e199b2da8e49de0a5c97b59706
Melnychuk Dmytro <2002mdo@gmail.com>
2024-12-12 02:13:25 +0200

    Added instructions

--- Scenes/main_scene.gd
+++ Scenes/main_scene.gd
@@ -1,6 +1,5 @@
 extends Node
 
-# Переменные
 var score = 0  # Очки игрока
 var current_document = {}  # Текущий документ
 var current_day = 1  # Текущий день
@@ -9,16 +8,84 @@ var DocumentScene = preload("res://Scenes/pasport.tscn")
 
 @onready var pasport: RigidBody2D = $Pasport
 
+# Функция для проверки возрастного диапазона
+func is_within_age_range(date_of_birth: String, min_age: int, max_age: int) -> bool:
+	var current_date = {"year": 2024, "month": 9, "day": 28}
+	var birth_date = date_of_birth.split("-")
+	var age = current_date.year - int(birth_date[0])
+	if current_date.month < int(birth_date[1]) or (current_date.month == int(birth_date[1]) and current_date.day < int(birth_date[2])):
+		age -= 1
+
+	print("Дата рождения:", date_of_birth, "Возраст:", age)
+	return min_age <= age and age <= max_age
+
+# Проверка документа по правилам и изменение очков
+func check_document(doc_data):
+	var rules = DocumentsData.rules[0]
+	var data = doc_data["actual_data"]
+	
+	# Проверяем категорию "Banned"
+	if "Banned" in rules:
+		for rule_title in rules["Banned"]:
+			var rule = rules["Banned"][rule_title]
+			if rule_title == "Military age men" and data["sex"] == "M":
+				if is_within_age_range(data["dateOfBirth"], 18, 60):
+					print("Document banned: Military age men -", data["firstName"], data["secondName"])
+					return false
+	
+	# Проверяем категорию "Permitted"
+	if "Permitted" in rules:
+		for rule_title in rules["Permitted"]:
+			var rule = rules["Permitted"][rule_title]
+			if rule_title == "Women and children under 18":
+				if data["sex"] == "F" and is_within_age_range(data["dateOfBirth"], 0, 17):
+					print("Document permitted: Women and children under 18 -", data["firstName"], data["secondName"])
+					score += 10
+					return true
+			elif rule_title == "Foreigners":
+				if data["citizenship"] != "UKR":
+					print("Document permitted: Foreigners -", data["firstName"], data["secondName"])
+					score += 5
+					return true
+	
+	# По умолчанию, если не подходит ни под одно правило
+	return false
+
 # Обновление UI
 func update_ui():
-	$MonitorControl/Score.text = "Очки: " + str(score)
+	$MonitorControl/ScoreField/Value.text = str(score)
 
 # Загрузка данных из глобального скрипта (если используется Singleton)
 func _ready():
 	score = 0  # Устанавливаем начальное значение очков
 	generate_new_document()
 	var modal = $vertdictControl/verdictButton/Popup
+	var info = $InformationControl
+	generate_information_text()
 	close_modal_window(modal)
+	close_modal_window(info)
+
+func generate_information_text():
+	var rules = DocumentsData.rules[0]  # Извлекаем первый объект правил
+	
+	var result_text = ""  # Строка для накопления текста
+	
+	# Обработка категории "Banned"
+	if "Banned" in rules:
+		result_text += "Banned:\n"
+		for title in rules["Banned"]:
+			var description = rules["Banned"][title]["description"]
+			result_text += "- " + title + ": " + description + "\n"
+	
+	# Обработка категории "Permitted"
+	if "Permitted" in rules:
+		result_text += "\nPermitted:\n"
+		for title in rules["Permitted"]:
+			var description = rules["Permitted"][title]["description"]
+			result_text += "- " + title + ": " + description + "\n"
+	
+	# Вывод текста в Label
+	$InformationControl/TextureRect/Label.text = result_text
 
 # Генерация нового документа
 func generate_new_document():
@@ -44,18 +111,13 @@ func generate_new_document():
 	$MonitorControl/DateOfBirth/Value.text = actual_data["dateOfBirth"]
 	$MonitorControl/ExpirationDate/Value.text = actual_data["expirationDate"]
 	$MonitorControl/Citizenship/Value.text = actual_data["citizenship"]
+	$MonitorControl/Sex/Value.text = actual_data["sex"]
 
 	var photo = load(actual_data["photo"])
 	$MonitorControl/PhotoField.texture = photo
 	
 	$Background/Person.texture = photo
 	pasport.set_data(actual_data)
-
-# Проверка документа
-func check_document(doc_data):
-	var fake = doc_data["fake_data"]
-	var actual = doc_data["actual_data"]
-	return fake == actual 
 
 # Переход на следующий день
 func next_day():
@@ -66,31 +128,36 @@ func next_day():
 
 # Настройка модального окна
 func close_modal_window(modal):
-	
 	modal.hide()
 
 func open_modal_window(modal):
-	
 	modal.show()
-
 
 func _on_accept_pressed() -> void:
 	$Click.play()
 	print("Документ принят")
-	var modal = $vertdictControl/verdictButton/Popup
-	close_modal_window(modal)
+	if check_document(current_document):
+		print("Правильное решение. Очки добавлены.")
+	else:
+		print("Ошибка! Документ не соответствует правилам.")
+	update_ui()
 	current_document_id += 1 
 	generate_new_document()
-
+	var modal = $vertdictControl/verdictButton/Popup
+	close_modal_window(modal)
 
 func _on_reject_pressed() -> void:
-	print("Кнопка нажата!")
 	$Click.play()
 	print("Документ отклонен")
-	var modal = $vertdictControl/verdictButton/Popup
-	close_modal_window(modal)
+	if not check_document(current_document):
+		print("Правильное решение. Очки добавлены.")
+	else:
+		print("Ошибка! Документ соответствует правилам.")
+	update_ui()
 	current_document_id += 1 
 	generate_new_document()
+	var modal = $vertdictControl/verdictButton/Popup
+	close_modal_window(modal)
 
 func _on_verdict_button_pressed() -> void:
 	$Click.play()
@@ -102,4 +169,15 @@ func _on_verdict_button_pressed() -> void:
 		open_modal_window(modal)
 
 func _on_exit_pressed():
+	$Click.play()
 	get_tree().quit()
+
+
+func _on_instruction_button_pressed() -> void:
+	$Click.play()
+	var modal = $InformationControl
+	
+	if modal.is_visible():
+		close_modal_window(modal)
+	else:
+		open_modal_window(modal)
--- Scripts/documents_data.gd
+++ Scripts/documents_data.gd
@@ -5,16 +5,16 @@ extends Node  # Или можно использовать Resource, если в
 var documents = [
 	{
 		"fake_data": {
-			"firstName": "Иван",
+			"firstName": "Galina",
 		},
 		"actual_data": {
-			"firstName": "Иван", 
-			"secondName": "Петров", 
-			"age": 34, 
-			"dateOfBirth": "2024-12-01", 
-			"expirationDate": "2025-12-01", 
-			"citizenship": "Украина",
-			"photo": "res://Assets/photos/female.png"
+			"firstName": "Galina", 
+			"secondName": "Tkachenko", 
+			"sex": "F",
+			"dateOfBirth": "1965-03-26", 
+			"expirationDate": "2028-12-01", 
+			"citizenship": "UKR",
+			"photo": "res://Assets/photos/1_grandma.png"
 		},
 		"day": 1
 	},
@@ -22,13 +22,13 @@ var documents = [
 		"fake_data": {
 		},
 		"actual_data": {
-			"firstName": "Анна", 
-			"secondName": "Смирнова", 
-			"age": 29, 
-			"dateOfBirth": "2024-11-15", 
-			"expirationDate": "2025-11-15", 
-			"citizenship": "Польша",
-			"photo": "res://Assets/photos/grandpa.png"
+			"firstName": "Oleksii", 
+			"secondName": "Tkachenko", 
+			"sex": "M" , 
+			"dateOfBirth": "1962-07-05", 
+			"expirationDate": "2027-11-15", 
+			"citizenship": "UKR",
+			"photo": "res://Assets/photos/2_grandpa.png"
 		},
 		"day": 1
 	},
@@ -36,14 +36,66 @@ var documents = [
 		"fake_data": {
 		},
 		"actual_data": {
-			"firstName": "Максим", 
-			"secondName": "Иванов", 
-			"age": 39, 
-			"dateOfBirth": "2024-12-05", 
-			"expirationDate": "2025-12-05", 
-			"citizenship": "Россия",
-			"photo": "res://Assets/photos/male.png"
+			"firstName": "Ivan", 
+			"secondName": "Mushko", 
+			"sex": "M" , 
+			"dateOfBirth": "1992-09-14", 
+			"expirationDate": "2038-12-05", 
+			"citizenship": "UKR",
+			"photo": "res://Assets/photos/3_male.png"
 		},
 		"day": 1
+	},
+{
+		"fake_data": {
+		},
+		"actual_data": {
+			"firstName": "Emilia", 
+			"secondName": "Harper", 
+			"sex": "F" , 
+			"dateOfBirth": "1990-17-11", 
+			"expirationDate": "2026-08-15", 
+			"citizenship": "UK",
+			"photo": "res://Assets/photos/4_female2.png"
+		},
+		"day": 1
+},
+{
+		"fake_data": {
+		},
+		"actual_data": {
+			"firstName": "Katerina", 
+			"secondName": "Kovalenko", 
+			"sex": "F" , 
+			"dateOfBirth": "2003-09-18", 
+			"expirationDate": "2031-04-10", 
+			"citizenship": "UKR",
+			"photo": "res://Assets/photos/5_female.png"
+		},
+		"day": 1
+}
+]
+var rules = [
+	{
+		"Banned": {
+			"Military age men": {
+				"rule": "18-60",
+				"description": "Men of military age (18-60) are not allowed to leave."
+			}
+		},
+		"Permitted": {
+			"Women and children under 18": {
+				"rules": ["F", "<18"],
+				"description": "Women and children under 18 years of age with valid documents are allowed to leave."
+			},
+			"Foreigners": {
+				"rules": ["!Ukr"],
+				"description": "All foreigners are allowed to leave."
+			},
+			"Extra note": {
+				"rules": [],
+				"description": "Check carefully for a match with external data."
+			}
+		}
 	}
 ]
